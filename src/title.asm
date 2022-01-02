7406e6754b90123ffd74a374b6a41442595759b8
Matthew D. Steele <mdsteele@alum.mit.edu>
2022-01-02 16:31:13 -0500

    Add charmap for text

--- src/title.asm
+++ src/title.asm
@@ -17,16 +17,70 @@
 ;;; with Annalog.  If not, see <http://www.gnu.org/licenses/>.              ;;;
 ;;;=========================================================================;;;
 
+.INCLUDE "charmap.inc"
+.INCLUDE "macros.inc"
+.INCLUDE "ppu.inc"
+
+.IMPORT Func_ClearRestOfOam
 .IMPORT Func_ProcessFrame
+.IMPORTZP Zp_OamOffset_u8
+.IMPORTZP Zp_Render_bPpuMask
+.IMPORTZP Zp_ScrollX_u8
+.IMPORTZP Zp_ScrollY_u8
 
 ;;;=========================================================================;;;
 
 .SEGMENT "PRG8_Title"
 
+.PROC Data_TitleString_u8_arr
+    .byte "ANNALOG"
+End:
+.ENDPROC
+
+;;; @prereq Rendering is disabled.
 .EXPORT Main_Title
 .PROC Main_Title
+    ;; Set up screen settings:
+    lda #bPpuMask::BgMain | bPpuMask::ObjMain
+    sta Zp_Render_bPpuMask
+    lda #0
+    sta Zp_ScrollX_u8
+    sta Zp_ScrollY_u8
+    ;; Clear OAM:
+    sta Zp_OamOffset_u8
+    jsr Func_ClearRestOfOam
+_ClearNametable0:
+    ldax #Ppu_Nametable0_sName + sName::Tiles_u8_arr
+    bit Hw_PpuStatus_ro  ; reset the Hw_PpuAddr_w2 write-twice latch
+    sta Hw_PpuAddr_w2
+    stx Hw_PpuAddr_w2
+    lda #0
+    ldxy #kScreenWidthTiles * kScreenHeightTiles
+    @loop:
+    sta Hw_PpuData_rw
+    dey
+    bne @loop
+    dex
+    bpl @loop
+_DrawTitleString:
+    .linecont +
+    ldax #Ppu_Nametable0_sName + sName::Tiles_u8_arr + \
+          kScreenWidthTiles * 14 + 12
+    .linecont -
+    bit Hw_PpuStatus_ro  ; reset the Hw_PpuAddr_w2 write-twice latch
+    sta Hw_PpuAddr_w2
+    stx Hw_PpuAddr_w2
+    ldy #Data_TitleString_u8_arr::End - Data_TitleString_u8_arr
+    ldx #0
+    @loop:
+    lda Data_TitleString_u8_arr, x
+    sta Hw_PpuData_rw
+    inx
+    dey
+    bne @loop
+_GameLoop:
     jsr Func_ProcessFrame
-    jmp Main_Title
+    jmp _GameLoop
 .ENDPROC
 
 ;;;=========================================================================;;;
