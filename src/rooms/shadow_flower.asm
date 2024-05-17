daa6329bc9f5c95e1913857b6ecc2a1c3691ab8f
Matthew D. Steele <mdsteele@alum.mit.edu>
2024-05-17 15:08:33 -0400

    Populate ShadowFlower room

--- src/rooms/shadow_flower.asm
+++ src/rooms/shadow_flower.asm
@@ -18,14 +18,54 @@
 ;;;=========================================================================;;;
 
 .INCLUDE "../actor.inc"
+.INCLUDE "../charmap.inc"
 .INCLUDE "../device.inc"
+.INCLUDE "../devices/flower.inc"
+.INCLUDE "../flag.inc"
+.INCLUDE "../machine.inc"
+.INCLUDE "../machines/laser.inc"
 .INCLUDE "../macros.inc"
 .INCLUDE "../platform.inc"
+.INCLUDE "../ppu.inc"
+.INCLUDE "../program.inc"
 .INCLUDE "../room.inc"
 
 .IMPORT DataA_Room_Shadow_sTileset
+.IMPORT FuncA_Machine_GenericMoveTowardGoalHorz
+.IMPORT FuncA_Machine_GenericTryMoveX
+.IMPORT FuncA_Machine_LaserTryAct
+.IMPORT FuncA_Machine_LaserWriteReg
+.IMPORT FuncA_Machine_ReachedGoal
+.IMPORT FuncA_Objects_DrawLaserMachine
+.IMPORT FuncA_Room_HarmAvatarIfWithinLaserBeam
+.IMPORT FuncA_Room_RemoveFlowerDeviceIfCarriedOrDelivered
+.IMPORT FuncA_Room_RespawnFlowerDeviceIfDropped
+.IMPORT Func_MachineLaserReadRegC
 .IMPORT Func_Noop
+.IMPORT Func_SetMachineIndex
+.IMPORT Func_WriteToUpperAttributeTable
 .IMPORT Ppu_ChrObjShadow
+.IMPORT Ram_MachineGoalHorz_u8_arr
+.IMPORT Ram_PlatformLeft_i16_0_arr
+
+;;;=========================================================================;;;
+
+;;; The machine index for the ShadowFlowerLaser machine in this room.
+kLaserMachineIndex = 0
+
+;;; The primary platform index for the ShadowFlowerLaser machine.
+kLaserPlatformIndex = 0
+
+;;; The initial and maximum permitted horizontal goal values for the laser.
+kLaserInitGoalX = 9
+kLaserMaxGoalX = 9
+
+;;; The maximum and initial X-positions for the left of the laser platform.
+.LINECONT +
+kLaserMinPlatformLeft = $0020
+kLaserInitPlatformLeft = \
+    kLaserMinPlatformLeft + kLaserInitGoalX * kBlockWidthPx
+.LINECONT -
 
 ;;;=========================================================================;;;
 
@@ -40,8 +80,8 @@
     d_byte MinimapStartRow_u8, 13
     d_byte MinimapStartCol_u8, 3
     d_addr TerrainData_ptr, _TerrainData
-    d_byte NumMachines_u8, 0
-    d_addr Machines_sMachine_arr_ptr, 0
+    d_byte NumMachines_u8, 1
+    d_addr Machines_sMachine_arr_ptr, _Machines_sMachine_arr
     d_byte Chr18Bank_u8, <.bank(Ppu_ChrObjShadow)
     d_addr Ext_sRoomExt_ptr, _Ext_sRoomExt
     D_END
@@ -52,24 +92,73 @@ _Ext_sRoomExt:
     d_addr Actors_sActor_arr_ptr, _Actors_sActor_arr
     d_addr Devices_sDevice_arr_ptr, _Devices_sDevice_arr
     d_addr Passages_sPassage_arr_ptr, _Passages_sPassage_arr
-    d_addr Enter_func_ptr, Func_Noop
-    d_addr FadeIn_func_ptr, Func_Noop
-    d_addr Tick_func_ptr, Func_Noop
+    d_addr Enter_func_ptr, FuncA_Room_RemoveFlowerDeviceIfCarriedOrDelivered
+    d_addr FadeIn_func_ptr, FuncA_Terrain_ShadowFlower_FadeInRoom
+    d_addr Tick_func_ptr, FuncA_Room_ShadowTrap_TickRoom
     d_addr Draw_func_ptr, Func_Noop
     D_END
 _TerrainData:
 :   .incbin "out/rooms/shadow_flower.room"
     .assert * - :- = 17 * 15, error
+_Machines_sMachine_arr:
+:   .assert * - :- = kLaserMachineIndex * .sizeof(sMachine), error
+    D_STRUCT sMachine
+    d_byte Code_eProgram, eProgram::ShadowFlowerLaser
+    d_byte Breaker_eFlag, 0
+    d_byte Flags_bMachine, bMachine::MoveH | bMachine::Act | bMachine::WriteC
+    d_byte Status_eDiagram, eDiagram::Laser
+    d_word ScrollGoalX_u16, $0
+    d_byte ScrollGoalY_u8, $10
+    d_byte RegNames_u8_arr4, "C", 0, "X", 0
+    d_byte MainPlatform_u8, kLaserPlatformIndex
+    d_addr Init_func_ptr, FuncA_Room_ShadowFlowerLaser_InitReset
+    d_addr ReadReg_func_ptr, FuncC_Shadow_FlowerLaser_ReadReg
+    d_addr WriteReg_func_ptr, FuncA_Machine_LaserWriteReg
+    d_addr TryMove_func_ptr, FuncA_Machine_ShadowFlowerLaser_TryMove
+    d_addr TryAct_func_ptr, FuncA_Machine_ShadowFlowerLaser_TryAct
+    d_addr Tick_func_ptr, FuncA_Machine_ShadowFlowerLaser_Tick
+    d_addr Draw_func_ptr, FuncA_Objects_DrawLaserMachine
+    d_addr Reset_func_ptr, FuncA_Room_ShadowFlowerLaser_InitReset
+    D_END
+    .assert * - :- <= kMaxMachines * .sizeof(sMachine), error
 _Platforms_sPlatform_arr:
-:   ;; TODO
+:   ;; ShadowFlowerLaser machine:
+    .assert * - :- = kLaserPlatformIndex * .sizeof(sPlatform), error
+    D_STRUCT sPlatform
+    d_byte Type_ePlatform, ePlatform::Solid
+    d_word WidthPx_u16, kLaserMachineWidthPx
+    d_byte HeightPx_u8, kLaserMachineHeightPx
+    d_word Left_i16, kLaserInitPlatformLeft
+    d_word Top_i16,   $0020
+    D_END
+    ;; Acid:
+    D_STRUCT sPlatform
+    d_byte Type_ePlatform, ePlatform::Kill
+    d_word WidthPx_u16, $80
+    d_byte HeightPx_u8, $20
+    d_word Left_i16,  $0050
+    d_word Top_i16,   $00ca
+    D_END
     .assert * - :- <= kMaxPlatforms * .sizeof(sPlatform), error
     .byte ePlatform::None
 _Actors_sActor_arr:
-:   ;; TODO
+:   ;; TODO: fake flower baddie
     .assert * - :- <= kMaxActors * .sizeof(sActor), error
     .byte eActor::None
 _Devices_sDevice_arr:
-:   ;; TODO
+:   .assert * - :- = kFlowerDeviceIndex * .sizeof(sDevice), error
+    D_STRUCT sDevice
+    d_byte Type_eDevice, eDevice::Flower
+    d_byte BlockRow_u8, 11
+    d_byte BlockCol_u8, 2
+    d_byte Target_byte, eFlag::FlowerShadow
+    D_END
+    D_STRUCT sDevice
+    d_byte Type_eDevice, eDevice::Console
+    d_byte BlockRow_u8, 4
+    d_byte BlockCol_u8, 2
+    d_byte Target_byte, kLaserMachineIndex
+    D_END
     .assert * - :- <= kMaxDevices * .sizeof(sDevice), error
     .byte eDevice::None
 _Passages_sPassage_arr:
@@ -80,6 +169,73 @@ _Passages_sPassage_arr:
     d_byte SpawnAdjust_byte, 0
     D_END
     .assert * - :- <= kMaxPassages * .sizeof(sPassage), error
+.ENDPROC
+
+.PROC FuncC_Shadow_FlowerLaser_ReadReg
+    cmp #$e
+    beq _ReadX
+_ReadC:
+    jmp Func_MachineLaserReadRegC
+_ReadX:
+    lda Ram_PlatformLeft_i16_0_arr + kLaserPlatformIndex
+    sub #kLaserMinPlatformLeft - kTileHeightPx
+    div #kBlockWidthPx
+    rts
+.ENDPROC
+
+;;;=========================================================================;;;
+
+.SEGMENT "PRGA_Room"
+
+.PROC FuncA_Room_ShadowTrap_TickRoom
+    ldx #kLaserMachineIndex
+    jsr Func_SetMachineIndex
+    jsr FuncA_Room_HarmAvatarIfWithinLaserBeam
+    jmp FuncA_Room_RespawnFlowerDeviceIfDropped
+.ENDPROC
+
+.PROC FuncA_Room_ShadowFlowerLaser_InitReset
+    lda #kLaserInitGoalX
+    sta Ram_MachineGoalHorz_u8_arr + kLaserMachineIndex
+    rts
+.ENDPROC
+
+;;;=========================================================================;;;
+
+.SEGMENT "PRGA_Machine"
+
+.PROC FuncA_Machine_ShadowFlowerLaser_TryMove
+    lda #kLaserMaxGoalX  ; param: max goal horz
+    jmp FuncA_Machine_GenericTryMoveX
+.ENDPROC
+
+.PROC FuncA_Machine_ShadowFlowerLaser_TryAct
+    ldx Ram_MachineGoalHorz_u8_arr + kLaserMachineIndex
+    lda _LaserBottom_i16_0_arr, x  ; param: laser bottom (lo)
+    ldy #0                         ; param: laser bottom (hi)
+    jmp FuncA_Machine_LaserTryAct
+_LaserBottom_i16_0_arr:
+:   .byte $50, $c0, $90, $70, $c5, $70, $c5, $80, $c0, $30
+    .assert * - :- = kLaserMaxGoalX + 1, error
+.ENDPROC
+
+.PROC FuncA_Machine_ShadowFlowerLaser_Tick
+    ldax #kLaserMinPlatformLeft  ; param: min platform left
+    jsr FuncA_Machine_GenericMoveTowardGoalHorz  ; returns Z
+    jeq FuncA_Machine_ReachedGoal
+    rts
+.ENDPROC
+
+;;;=========================================================================;;;
+
+.SEGMENT "PRGA_Terrain"
+
+;;; @prereq Rendering is disabled.
+.PROC FuncA_Terrain_ShadowFlower_FadeInRoom
+    ldx #5    ; param: num bytes to write
+    ldy #$aa  ; param: attribute value
+    lda #$32  ; param: initial byte offset
+    jmp Func_WriteToUpperAttributeTable
 .ENDPROC
 
 ;;;=========================================================================;;;
