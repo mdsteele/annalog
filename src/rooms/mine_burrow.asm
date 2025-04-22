24c5e13c992b6e5c604f6009bac4539c1d779102
Matthew D. Steele <mdsteele@alum.mit.edu>
2025-04-22 15:22:03 -0400

    Rumble the room as the player avatar traverses MineBurrow

--- src/rooms/mine_burrow.asm
+++ src/rooms/mine_burrow.asm
@@ -29,17 +29,46 @@
 
 .IMPORT DataA_Room_Mine_sTileset
 .IMPORT FuncA_Objects_AnimateCircuitIfBreakerActive
+.IMPORT FuncA_Room_PlaySfxRumbling
+.IMPORT Func_IsPointInPlatform
 .IMPORT Func_Noop
+.IMPORT Func_SetPointToAvatarCenter
+.IMPORT Func_ShakeRoom
 .IMPORT Int_SetChr04ToParam3ThenLatchWindowFromParam4
 .IMPORT Ppu_ChrObjMine
+.IMPORT Sram_ProgressFlags_arr
 .IMPORTZP Zp_Buffered_sIrq
 .IMPORTZP Zp_Chr04Bank_u8
+.IMPORTZP Zp_RoomState
 
 ;;;=========================================================================;;;
+
+;;; How many frames to rumble the room for when the player avatar enters a new
+;;; rumble zone.
+kRumbleFrames = 120
 
 ;;; The screen pixel Y-position at which the water IRQ should change the CHR04
 ;;; bank.
 kWaterChr04IrqY = $80
+
+;;; One of the three rumble zones in this room.
+.ENUM eRumble
+    West
+    Center
+    East
+    NUM_VALUES
+.ENDENUM
+
+;;;=========================================================================;;;
+
+;;; Defines room-specific state data for this particular room.
+.STRUCT sState
+    ;; The rumble zone that the player avatar most recently entered.
+    Last_eRumble   .byte
+    ;; How many more frames to rumble for.
+    RumbleTimer_u8 .byte
+.ENDSTRUCT
+.ASSERT .sizeof(sState) <= kRoomStateSize, error
 
 ;;;=========================================================================;;;
 
@@ -68,14 +97,41 @@ _Ext_sRoomExt:
     d_addr Passages_sPassage_arr_ptr, _Passages_sPassage_arr
     d_addr Enter_func_ptr, Func_Noop
     d_addr FadeIn_func_ptr, Func_Noop
-    d_addr Tick_func_ptr, Func_Noop
+    d_addr Tick_func_ptr, FuncC_Mine_Burrow_TickRoom
     d_addr Draw_func_ptr, FuncC_Mine_Burrow_DrawRoom
     D_END
 _TerrainData:
 :   .incbin "out/rooms/mine_burrow.room"
     .assert * - :- = 33 * 15, error
 _Platforms_sPlatform_arr:
-:   D_STRUCT sPlatform
+:   ;; Rumble zones, indexed by eRumble:
+    .assert * - :- <= eRumble::West * .sizeof(sPlatform), error
+    D_STRUCT sPlatform
+    d_byte Type_ePlatform, ePlatform::Zone
+    d_word WidthPx_u16, $90
+    d_byte HeightPx_u8, $f0
+    d_word Left_i16,  $0020
+    d_word Top_i16,   $0000
+    D_END
+    .assert * - :- <= eRumble::Center * .sizeof(sPlatform), error
+    D_STRUCT sPlatform
+    d_byte Type_ePlatform, ePlatform::Zone
+    d_word WidthPx_u16, $40
+    d_byte HeightPx_u8, $90
+    d_word Left_i16,  $00e0
+    d_word Top_i16,   $0060
+    D_END
+    .assert * - :- <= eRumble::East * .sizeof(sPlatform), error
+    D_STRUCT sPlatform
+    d_byte Type_ePlatform, ePlatform::Zone
+    d_word WidthPx_u16, $60
+    d_byte HeightPx_u8, $50
+    d_word Left_i16,  $0170
+    d_word Top_i16,   $0010
+    D_END
+    .assert * - :- <= eRumble::NUM_VALUES * .sizeof(sPlatform), error
+    ;; Water:
+    D_STRUCT sPlatform
     d_byte Type_ePlatform, ePlatform::Water
     d_word WidthPx_u16, $40
     d_byte HeightPx_u8, $20
@@ -150,6 +206,46 @@ _Passages_sPassage_arr:
     d_byte SpawnAdjust_byte, 0
     D_END
     .assert * - :- <= kMaxPassages * .sizeof(sPassage), error
+.ENDPROC
+
+;;; @prereq PRGA_Room is loaded.
+.PROC FuncC_Mine_Burrow_TickRoom
+_RumbleRoom:
+    lda Zp_RoomState + sState::RumbleTimer_u8
+    beq @done
+    dec Zp_RoomState + sState::RumbleTimer_u8
+    mod #4
+    bne @done
+    lda #2  ; param: num frames
+    jsr Func_ShakeRoom
+    lda #4  ; param: num frames
+    jsr FuncA_Room_PlaySfxRumbling
+    @done:
+_CheckForNewRumbleZone:
+    ;; Once the boss has been defeated, don't rumble anymore.
+    flag_bit Sram_ProgressFlags_arr, eFlag::BossMine
+    bne _Return
+    ;; Find the rumble zone that the player avatar is in, if any.
+    jsr Func_SetPointToAvatarCenter
+    ldy #eRumble::NUM_VALUES - 1
+    @loop:
+    jsr Func_IsPointInPlatform  ; preserves Y
+    bcs _MaybeStartRumbling
+    dey
+    .assert eRumble::NUM_VALUES <= $80, error
+    bpl @loop
+    ;; If the player avatar is not in any rumble zone, we're done.
+_Return:
+    rts
+_MaybeStartRumbling:
+    ;; If the player avatar is in the same rumble zone as before, do nothing.
+    cpy Zp_RoomState + sState::Last_eRumble
+    beq _Return
+    ;; Otherwise, start rumbling.
+    sty Zp_RoomState + sState::Last_eRumble
+    lda #kRumbleFrames
+    sta Zp_RoomState + sState::RumbleTimer_u8
+    rts
 .ENDPROC
 
 ;;; @prereq PRGA_Objects is loaded.
