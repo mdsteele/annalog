9680bc15e5a0089b59e12b04f77c2e40d084298e
Matthew D. Steele <mdsteele@alum.mit.edu>
2025-07-05 18:43:55 -0700

    In CityDump, adjust upper grub start position to the left

--- src/rooms/city_dump.asm
+++ src/rooms/city_dump.asm
@@ -71,7 +71,7 @@ _Actors_sActor_arr:
     D_END
     D_STRUCT sActor
     d_byte Type_eActor, eActor::BadGrub
-    d_word PosX_i16, $00a0
+    d_word PosX_i16, $0098
     d_word PosY_i16, $0088
     d_byte Param_byte, 0
     D_END
